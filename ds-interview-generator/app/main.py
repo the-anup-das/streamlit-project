08fffce45342465c20a7f7dc31d969ac4126c7e8
the-anup-das <anup.das17@yahoo.com>
2022-08-10 15:49:35 +0530

    Fixing the file not found issue

--- ds-interview-generator/app/main.py
+++ ds-interview-generator/app/main.py
@@ -9,7 +9,7 @@ import os
 
 path = os.path.dirname(__file__)
 
-st.set_page_config(page_title='Data Science Interview practice', page_icon='/assets/icon.png')
+st.set_page_config(page_title='Data Science Interview practice', page_icon='assets/icon.png')
 # st.set_page_config(page_title='Data Science Interview practice', page_icon='assets/icon.png')
 
 ss = st.session_state
@@ -22,7 +22,7 @@ if 'prob_click' not in ss:
 if st.button('Bring it on!'):
     ss['prob_click'] = True
     ss['report_click'] = False
-    df = read_data(path+'//data/olympiad-problems.csv')
+    df = read_data(path+'data/olympiad-problems.csv')
     choice = df.sample(1)
     ss['sample'] = choice
     body(choice)
